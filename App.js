d33e098a1ab3260aad0e0844ebedb64f26320337
wudxoe <120737105+wudxoe@users.noreply.github.com>
2023-12-04 02:42:53 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-//import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MainScreen from './screens/MainScreen'; // MainScreen 컴포넌트를 별도의 파일로 분리해야 함
+import WhoScreen from './screens/WhoScreen';
 import LoginScreen from './screens/LoginScreen';
+import DLoginScreen from './screens/DLoginScreen';
 import SignupScreen from './screens/SignupScreen';
+import DSignupScreen from './screens/DSignupScreen';
 import HomeScreen from './screens/HomeScreen'; // HomeScreen 컴포넌트를 별도의 파일로 분리해야 함
 import SearchScreen from './screens/SearchScreen';
 import ReserveScreen from './screens/ReserveScreen';
 import MapScreen from './screens/MapScreen';
 import MyInfoScreen from './screens/MyInfoScreen';
+import MyProfileScreen from './screens/MyProfileScreen';
 import SettingsScreen from './screens/SettingsScreen';
 import ReservationListScreen from './screens/ReservationListScreen';
-import DLoginScreen from './screens/DLoginScreen';
+import DReservationListScreen from './screens/DReservationListScreen';
 
 const Stack = createStackNavigator();
 //const Tab = createBottomTabNavigator();
@@ -51,18 +55,24 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Main">
-        <Stack.Screen name="MyDesisgner" component={MainScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="MyDesisgner" component={MainScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Who" component={WhoScreen} options={{ title: 'Designer or Customer' }} />
         <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="SignUp" component={SignupScreen} />
+        <Stack.Screen name="DLogin" component={DLoginScreen} />
+        <Stack.Screen name="SignUp" component={SignupScreen} options={{ title: '회원가입' }} />
+        <Stack.Screen name="DSignUp" component={DSignupScreen} options={{ title: '회원가입' }} />
         {/* <Stack.Screen name="MainTab" component={MainTabScreen} options={{ headerShown: false }} /> */}
         <Stack.Screen name="Search" component={SearchScreen} />
         <Stack.Screen name="Reserve" component={ReserveScreen} />
         <Stack.Screen name="Map" component={MapScreen} />
-        <Stack.Screen name="MyInfo" component={MyInfoScreen} />
+        <Stack.Screen name="MyInfo" component={MyInfoScreen} options={{ title: '내정보' }} />
+        <Stack.Screen name="MyProfile" component={MyProfileScreen} options={{ title: '내정보' }} />
         <Stack.Screen name="Settings" component={SettingsScreen} />
         <Stack.Screen name="ReservationList" component={ReservationListScreen} />
-        <Stack.Screen name="DLogin" component={DLoginScreen} />
+        <Stack.Screen name="DReservationList" component={DReservationListScreen} />
+        <Stack.Screen name="MainTab" component={TabNavigator} options={{ headerShown: false }} />
+        <Stack.Screen name="MyInfoTab" component={TabNavigator} options={{ headerShown: true }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
