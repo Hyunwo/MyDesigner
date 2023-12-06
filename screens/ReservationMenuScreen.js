e23a51c766b89ced9d9e15b0c88a6a6cd9378985
wudxoe <120737105+wudxoe@users.noreply.github.com>
2023-12-06 09:33:32 +0900

    Add files via upload

---
+++ screens/ReservationMenuScreen.js
@@ -0,0 +1,118 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Image, ScrollView, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { doc, getDoc } from 'firebase/firestore';
+import { firestore } from '../config/firebaseConfig';
+
+const ReservationMenuScreen = ({ route, navigation }) => {
+    const { designerId } = route.params; // designerId를 route.params에서 추출
+  // 예시를 위해 정적으로 서비스 목록을 정의합니다.
+  // 실제 앱에서는 서버에서 데이터를 가져오거나 상태 관리를 통해 동적으로 목록을 구성해야 합니다.
+  const services = [
+    { id: '1', title: '학생 커트(0세~19세)', price: '17,000원', onPress: () => {} },
+    { id: '2', title: '남성 커트', price: '19,000원', onPress: () => {} },
+    { id: '3', title: '여성 커트', price: '20,000원', onPress: () => {} },
+    // 추가 서비스 아이템들...
+  ];
+
+  const handleServicePress = (service) => {
+    navigation.navigate('Studentcut');
+    // 예약 화면으로 네비게이션하거나 서비스 관련 액션을 실행합니다.
+    // navigation.navigate('ReservationScreen', { serviceId: service.id });
+    Alert.alert('Service Selected', `You selected ${service.title}`);
+  };
+
+  return (
+    <ScrollView style={styles.container}>
+      {/* ... 프로필 이미지, 이름, 타이틀, 설명 ... */}
+      <View style={styles.menuSection}>
+        <Text style={styles.menuTitle}>커트</Text>
+        {services.map((service) => (
+          <TouchableOpacity key={service.id} style={styles.menuItem} onPress={() => handleServicePress(service)}>
+            <Text style={styles.menuItemText}>{service.title}</Text>
+            <View style={styles.menuItemRight}>
+              <Text style={styles.menuItemPrice}>{service.price}</Text>
+              <Ionicons name="chevron-forward-outline" size={24} color="#007bff" />
+            </View>
+          </TouchableOpacity>
+        ))}
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+      },
+      profileImage: {
+        width: 120,
+        height: 120,
+        borderRadius: 60,
+        alignSelf: 'center',
+        marginTop: 32,
+      },
+      name: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        marginTop: 16,
+        textAlign: 'center',
+      },
+      title: {
+        textAlign: 'center',
+        color: 'gray',
+        marginTop: 4,
+      },
+      salon: {
+        textAlign: 'center',
+        color: 'gray',
+        marginTop: 4,
+      },
+      description: {
+        textAlign: 'center',
+        color: 'gray',
+        marginTop: 4,
+        marginBottom: 16,
+      },
+      interactionRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        paddingVertical: 16,
+      },
+      interactionButton: {
+        alignItems: 'center',
+      },
+      interactionText: {
+        fontSize: 16,
+      },
+      menuSection: {
+        paddingHorizontal: 32,
+        paddingTop: 16,
+      },
+      menuTitle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        paddingBottom: 16,
+      },
+      menuItem: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 8,
+        borderBottomWidth: 1,
+        borderBottomColor: '#e1e1e1',
+      },
+      menuItemText: {
+        fontSize: 18,
+      },
+      menuItemRight: {
+        flexDirection: 'row',
+        alignItems: 'center',
+      },
+      menuItemPrice: {
+        fontSize: 18,
+        color: '#007bff',
+      },
+});
+
+export default ReservationMenuScreen;
---
+++ screens/StudentcutScreen.js
@@ -0,0 +1,122 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image, ScrollView, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+const StudentcutScreen = () => {
+  // Assuming we have a state to track the number of likes
+  const [likes, setLikes] = useState(0);
+
+  const onLikePress = () => {
+    // This would be your like handling logic, for now it's just incrementing a counter
+    setLikes(likes + 1);
+  };
+
+  return (
+    <ScrollView style={styles.container}>
+      <View style={styles.header}>
+        {/* Like and share icons */}
+        <Ionicons name="heart-outline" size={24} color="black" onPress={onLikePress} />
+        <Text style={styles.likes}>{likes}</Text>
+        <Ionicons name="share-social-outline" size={24} color="black" />
+      </View>
+      
+      <Text style={styles.serviceTitle}>학생 커트(0세-19세)</Text>
+      <Text style={styles.price}>17,000</Text>
+      <Text style={styles.salonName}>@어울리다 배방점</Text>
+      
+      {/* Service Details */}
+      <View style={styles.details}>
+        <Text style={styles.detailTitle}>정보</Text>
+        <View style={styles.detailRow}>
+          <Ionicons name="time-outline" size={20} color="black" />
+          <Text style={styles.detailText}>30분 소요</Text>
+        </View>
+        <View style={styles.detailRow}>
+          <Ionicons name="information-circle-outline" size={20} color="black" />
+          <Text style={styles.detailText}>부가정보</Text>
+        </View>
+        {/* ... More details here ... */}
+        <Text style={styles.description}>
+          가장 중요하고 기본이 되는 커트를 고객님과 정확한 상담을 통해 만족스러운 디자인으로 도와드리겠습니다.
+          {'\n'}*단발기장이상 샴푸 +3,000원(선택)
+        </Text>
+      </View>
+
+      {/* Booking Button */}
+      <TouchableOpacity style={styles.bookingButton} onPress={() => navigation.navigate('StudentcutReservation')}>
+        <Text style={styles.bookingButtonText}>예약하기</Text>
+      </TouchableOpacity>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 16,
+  },
+  likes: {
+    fontSize: 16,
+  },
+  serviceTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  price: {
+    fontSize: 22,
+    color: '#E91E63',
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  salonName: {
+    textAlign: 'center',
+    fontSize: 16,
+    color: 'grey',
+    marginBottom: 16,
+  },
+  details: {
+    paddingHorizontal: 16,
+  },
+  detailTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  detailRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 8,
+  },
+  detailText: {
+    marginLeft: 8,
+    fontSize: 16,
+  },
+  description: {
+    fontSize: 16,
+    color: 'grey',
+    marginTop: 8,
+  },
+  bookingButton: {
+    backgroundColor: 'red',
+    padding: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 8,
+    marginVertical: 16,
+    marginHorizontal: 16,
+  },
+  bookingButtonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
+
+export default StudentcutScreen;
