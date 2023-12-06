5e0ea7ab914de29d75044c7750a4377514a2359b
wudxoe <120737105+wudxoe@users.noreply.github.com>
2023-12-06 15:12:54 +0900

    Update MainScreen.js

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, Image } from 'react-native';
 
 function MainScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <Text style={styles.header}>MyDesigner</Text>
 
-      <TouchableOpacity
-        style={[styles.button, styles.firstButton]}
-        onPress={() => navigation.navigate('Login')}
-      >
-        <Text style={styles.buttonText}>디자이너</Text>
-      </TouchableOpacity>
+      <Image
+        source={require('../assets/s.png')}
+        style={styles.logo}
+      />
 
       <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate('Login')}
+        style={[styles.button, styles.firstButton]}
+        onPress={() => navigation.navigate('Who')}
       >
-        <Text style={styles.buttonText}>고객</Text>
+        <Text style={styles.buttonText}>시작하기</Text>
       </TouchableOpacity>
     </View>
   );
@@ -36,10 +34,16 @@ const styles = StyleSheet.create({
     fontSize: 24,
     marginBottom: 30, // 헤더와 버튼 사이 간격 조정
   },
+  logo: {
+    width: 100, // 원하는 크기로 조정
+    height: 100, // 원하는 크기로 조정
+    resizeMode: 'contain', // 이미지가 컨테이너에 맞도록 조정
+    marginBottom: 20, // 헤더와 이미지 사이 간격 조정
+  },
   button: {
     height: 50,
     width: '60%', // 버튼 길이 조정
-    backgroundColor: 'white',
+    backgroundColor: 'skyblue',
     borderWidth: 1,
     borderColor: '#007AFF',
     borderRadius: 10,
@@ -51,7 +55,7 @@ const styles = StyleSheet.create({
     marginTop: 0, // 첫 번째 버튼의 상단 마진을 없애기
   },
   buttonText: {
-    color: '#007AFF',
+    color: 'black',
     fontSize: 18,
   },
 });
